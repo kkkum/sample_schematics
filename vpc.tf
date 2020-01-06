319c8f3f24ea1b3e49a44ad0a58ad8009986dd23
Kwok Kee Kum <kwokkeekum@kwoks-mbp.home>
2020-01-06 22:11:35 +1100

    Corrected ubuntu image

--- vpc.tf
+++ vpc.tf
@@ -40,7 +40,7 @@ resource ibm_is_subnet "subnet1" {
 }
 
 data ibm_is_image "ubuntu" {
-  name = "ibm-ubuntu-18-04-3-minimal-ppc64le-2"
+  name = "ibm-ubuntu-18-04-1-minimal-amd64-1"
 }
 
 data ibm_is_ssh_key "ssh_key_id" {
